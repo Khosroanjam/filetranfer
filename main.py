6421525e87724cfa486c43db14052bbcadc591e5
Sadegh Khosroanjam <khosroanjam@gmail.com>
2021-10-10 19:27:57 +0330

    class makeUrl added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
+from model import sqlUtil, makeUrl
 from werkzeug.utils import secure_filename
 import os
 
@@ -8,50 +8,12 @@ UPLOAD_FOLDER = 'upload'
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'kh0sr0@n0m'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-db = SQLAlchemy(app)
-
-class urls(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    users = db.relationship('users', backref=db.backref('urls', lazy=True))
-    filename = db.Column(db.Text)
-    urladdress = db.Column(db.Text)
-    haspassword = db.Column(db.Text)
-    password = db.Column(db.Text)
-    active = db.Column(db.Text)
-    reserve1 = db.Column(db.Text)
-    reserve2 = db.Column(db.Text)
-    def __init__(self, userid , filename, urladdress, active, haspassword, password):
-        self.userid = userid,
-        self.filename = filename,
-        self.urladdress = urladdress,
-        self.haspassword = haspassword,
-        self.password = password,
-        self.active = active,
-        self.reserve1 = "",
-        self.reserve2 = ""
-    def __repr__(self):
-        return self.urladdress
-    def __url__(self):
-        return self.urladdress
-    def __Str__(self):
-        return self.filename
 
 
-class  users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text)
-    family = db.Column(db.Text)
-    username = db.Column(db.Text)
-    password = db.Column(db.Text)
-    active = db.Column(db.Boolean)
-    def __username__(self):
-        return str(self.username)
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
+    u = makeUrl()
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('NO FILE')
@@ -62,9 +24,8 @@ def main():
             return redirect(request.url)
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        u = urls(userid=1, filename = str(filename), urladdress = "qwe", haspassword=str('false'), password="", active=str('true'))
-        db.session.add(u)
-        db.session.commit()
+        s = sqlUtil(userid=1, filename = str(filename), urladdress = u.make(), haspassword=str('false'), password="", active=str('true'))
+        s.insert()
         return redirect(url_for('success'))
 
     return render_template('home.html')
--- model.py
+++ model.py
@@ -1,13 +1,17 @@
 import sqlite3 as sql
-
+import random as ra
 DBPATH = 'database.db'
 
 con = sql.connect(DBPATH)
 cur =  con.cursor()
 
 
+lower = ['a', 'b',	'c',	'd',	'e',	'f',	'g',	'h',	'i',	'j',	'k',	'l',	'm',	'n',	'o',	'p',	'q',	'r',	's',	't',	'u',	'v',	'w',	'x',	'y',	'z']
+upper = ['A',	'B',	'C',	'D',	'E',	'F',	'G',	'H',	'I',	'J',	'K',	'L',	'M',	'N',	'O',	'P',	'Q',	'R',	'S',	'T',	'U',	'V',	'W',	'X',	'Y',	'Z']
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-class sqlutil():
+
+class sqlUtil():
     def __init__(self, userid, filename, urladdress, haspassword, password, active):
         self.userid = userid
         self.filename = filename
@@ -18,12 +22,24 @@ class sqlutil():
         self.dbpath = 'database.db'
         self.con = sql.connect('database.db')
         self.cur = self.con
+        self.SQL_COMMAND_INSERT = ''
     def insert(self):
-        SQL_COMMAND_INSERT  = '''INSERT INTO urls (userid, filename, urladdress, haspassword, password, active) 
-             VALUES (self.userid, self.filename, self.urladdress, self.haspassword, self.password, self.active)'''
+        print(self.filename)
+        self.SQL_COMMAND_INSERT  = "INSERT INTO urls(userid, filename, urladdress, haspassword, password, active) VALUES ({0}, '{1}', '{2}', '{3}', '{4}', '{5}')".format(self.userid, self.filename, self.urladdress, self.haspassword, self.password, self.active)
         print(self.SQL_COMMAND_INSERT)
-        self.cur.execute(SQL_COMMAND_INSERT)
+        self.cur.execute(self.SQL_COMMAND_INSERT)
         self.con.commit()
         self.con.close()
 
-        a = sqlutil(1,'qq.exe', 'ASwwaw', 'false','','true')
+class makeUrl():
+    def __init__(self):
+        lower = ['a', 'b',	'c',	'd',	'e',	'f',	'g',	'h',	'i',	'j',	'k',	'l',	'm',	'n',	'o',	'p',	'q',	'r',	's',	't',	'u',	'v',	'w',	'x',	'y',	'z']
+        upper = ['A',	'B',	'C',	'D',	'E',	'F',	'G',	'H',	'I',	'J',	'K',	'L',	'M',	'N',	'O',	'P',	'Q',	'R',	'S',	'T',	'U',	'V',	'W',	'X',	'Y',	'Z']
+        number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    def make(self):
+        s = str()
+        for i in range(2):
+            s = s + lower[ra.randint(0,25)]
+            s = s + upper[ra.randint(0,25)]
+            s = s + str(number[ra.randint(0,9)])
+        return s    
